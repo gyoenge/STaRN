8018dde2b2a26f5ba6b40017326ebd90081773bd
gyoenge <gyoeng94@gmail.com>
2026-05-26 19:41:27 +0900

    feat: add init model

---
+++ model/tabular.py
@@ -0,0 +1,18 @@
+# Variant of TransTab + SAINT 
+
+import torch 
+import torch.nn as nn 
+
+
+class ColumnAttention(nn.Module):
+    pass 
+
+
+class RowAttention(nn.Module):
+    pass
+
+
+class SummaryTableModel(nn.Module): 
+    pass 
+
+
